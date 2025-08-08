64f85ae17d2db8e52c9af0f205174367e3c3ffea
Alloystar <alexander.clemas@gmail.com>
2025-08-08 15:12:28 +1200

    pausee menu fixed
    thank mr cave

--- Creatures/character.gd
+++ Creatures/character.gd
@@ -1,28 +1,14 @@
 extends CharacterBody2D
 
-@onready var pause_menu = $PauseMenu
-var paused = false
+
+
 var speed = 200
 func _process(_delta):
 	var direction = Input.get_vector("Left","Right","Up","Down")
 	velocity = direction * speed
 	move_and_slide()
 	
-#pause menu (from a youtube tutorial from Gwizz
-	if Input.is_action_just_pressed("Pause"):
-		pausemenu()
-		
-func pausemenu():
-	var last_mouse_direction = get_global_mouse_position()
-	if paused:
-		pause_menu.hide()
-		Engine.time_scale = 1
-	else:
-		look_at(last_mouse_direction)
-		pause_menu.show()
-		Engine.time_scale = 0
-	
-	paused = !paused
+
 func get_last_mouse_pos():
 	pass
 
--- Menus/pause_menu.gd
+++ Menus/pause_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 @export var start_game:String
 @onready var main = $"../"
-
+var paused = false
 func _on_settings_pressed():
 	pass # Replace with function body.
 
@@ -9,7 +9,24 @@ func _on_settings_pressed():
 func _on_quit_pressed():
 	get_tree().call_deferred("change_scene_to_file",start_game)
 	Engine.time_scale = 1
-
+	
+	
+func _process(_delta):
+	print("buh")
+	#pause menu (from a youtube tutorial from Gwizz
+	if Input.is_action_just_pressed("Pause"):
+		pausemenu()
+		
+func pausemenu():
+	print(paused)
+	if paused:
+		$"..".hide()
+		Engine.time_scale = 1
+	else:
+		$"..".show()
+		Engine.time_scale = 0
+	
+	paused = !paused
 
 func _on_resume_pressed():
-	main.pausemenu()
+	pausemenu()
