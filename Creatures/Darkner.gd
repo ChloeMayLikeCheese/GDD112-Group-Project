e8ebad1eb8d2ebb8cb8b1dcae4d24304492b2ccb
Alloystar <alexander.clemas@gmail.com>
2025-08-15 12:55:09 +1200

    dark boi go

--- Creatures/Darkner.gd
+++ Creatures/Darkner.gd
@@ -1,10 +1,20 @@
 extends CharacterBody2D
 
-func _process(_delta):
-	pass
-
-
+var move_speed = -500
+var chasing = false
+func _physics_process(delta):
+	if chasing:
+		#global_position.move_toward($"../../Character".global_position, delta * 100)
+		
+		var player = $"../../Character"
+		
+		velocity = (self.global_position - player.global_position).normalized() * move_speed
+		move_and_slide()
+		
 
 func interact_with_light():
 	visible = false
 	$CollisionShape2D.disabled = true
+
+func _on_timer_timeout():
+	chasing = true
