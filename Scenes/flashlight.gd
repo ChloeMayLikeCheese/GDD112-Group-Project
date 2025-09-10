9d65586069b011d3b3765198a6829cf89c66a38e
Alloystar <alexander.clemas@gmail.com>
2025-09-11 09:39:06 +1200

    Added breather functionality

--- Scenes/flashlight.gd
+++ Scenes/flashlight.gd
@@ -7,6 +7,13 @@ signal breather_flashlight
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	look_at(get_global_mouse_position())
+	for body in $".".get_overlapping_bodies():
+		if not body is CharacterBody2D:
+			continue
+		print(body.name)
+		if body.has_method("in_flashlight"):
+			print("bodyaaaa it byrnswd")
+			body.in_flashlight()
 
 
 func _on_area_shape_entered(_area_rid, _area, _area_shape_index, _local_shape_index):
