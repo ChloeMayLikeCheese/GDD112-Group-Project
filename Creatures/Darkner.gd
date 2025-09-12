db814008b998ef6b1875ddaa0b44a0ed49f61f14
Alloystar <alexander.clemas@gmail.com>
2025-09-12 14:59:22 +1200

    death from darkner added

--- Creatures/Darkner.gd
+++ Creatures/Darkner.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 signal flashlight
-var move_speed = -100
+var move_speed = -230
 var chasing = false
 func _physics_process(_delta):
 #the magic that makes the evil and conniving monster move!
@@ -18,9 +18,15 @@ func _on_timer_timeout():
 func _on_character_flashlight():
 	$Timer2.start()
 	chasing = false
+	
 
 #Starting on a Interjacent flashlight's(TM) contact with the entity this timer does uhhm something? i think?
 func _on_timer_2_timeout():
 	visible = false
 	$Area2D/CollisionShape2D.disabled = true
 	
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Character":
+		get_tree().call_deferred("change_scene_to_file","res://Menus/game_over.tscn")
